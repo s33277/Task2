5d19a1a427ed9f637a8c94e9cd1609e8e4cce7c4
s33277 <s33277@pjwstk.edu.pl>
2026-03-23 23:12:45 +0100

    Create class Tablet

---
+++ Models/Tablet.cs
@@ -0,0 +1,15 @@
+﻿namespace APBD_TASK2.Models
+{
+    public class Tablet : Equipment
+    {
+        public bool HasStylus { get; set; }
+        public string OperatingSystem { get; set; }
+
+        public Tablet(string name, bool hasStylus, string os) 
+            : base(name)
+        {
+            HasStylus = hasStylus;
+            OperatingSystem = os;
+        }
+    }
+}
