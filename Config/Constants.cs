0579a195cf739554c14eb8b4c81faf6402eca508
s33277 <s33277@pjwstk.edu.pl>
2026-03-23 22:37:56 +0100

    Create Constants class with business constants

---
+++ Config/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace APBD_TASK2.Config;
+
+public class Constants
+{
+    public const decimal DailyLatePenalty = 10m;
+    public const int StandardRentalDays = 30;
+        
+    public const int StudentRentalLimit = 2;
+    public const int EmployeeRentalLimit = 5;
+        
+}
