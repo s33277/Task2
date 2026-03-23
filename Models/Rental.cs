c5f2b004f2e5ab8c88961dcebb453dd821b9faf8
s33277 <s33277@pjwstk.edu.pl>
2026-03-23 21:40:44 +0100

    Configure class Rental and create method CalculatePenalty

--- Models/Rental.cs
+++ Models/Rental.cs
@@ -2,5 +2,16 @@
 
 public class Rental
 {
-    
+    public User User { get; set; }
+    public Equipment Equipment { get; set; }
+    public DateTime RentalDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+
+    public decimal CalculatePenalty()
+    {
+        if (!ReturnDate.HasValue | ReturnDate <= DueDate) return 0;
+        TimeSpan delay = ReturnDate.Value - DueDate;
+        return delay.Days*4;
+    }
 }
